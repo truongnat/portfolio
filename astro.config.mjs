88c868b1fa3c702fb18e9a8356e9a80b71a8993e
Peanut <truongdq.dev@gmail.com>
2026-03-18 23:35:51 +0700

    fix: revert output to static for Astro 6 compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://truongdq.com',
-  output: 'hybrid',
+  output: 'static',
   adapter: node({
     mode: 'standalone'
   }),
